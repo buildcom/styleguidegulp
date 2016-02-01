d21d07b71574e7b16e1e868941caee700cd3261e
maguilar <maguilar@livingdirect.com>
2016-02-01 11:03:49 -0600

    Initial Commit

---
+++ gulpfile.js
@@ -0,0 +1,66 @@
+'use strict';
+
+var gulp = require('gulp'),
+	watch = require('gulp-watch'),
+	plumber = require('gulp-plumber'),
+	gutil = require('gulp-util'),
+    hologram = require('gulp-hologram');
+
+var paths = {
+    shared: {
+        src: './Source/Shared'
+    },
+    ca: {
+        src: './Source/CA',
+        work: './Working/CA',
+        holo: './hologram_config_ca.yml'
+    },
+    keg: {
+        src: './Source/Keg',
+        work: './Working/Keg',
+        holo: './hologram_config_keg.yml'
+    }
+}
+
+var onError = function (err) {  
+  gutil.beep();
+  console.log(err);
+};
+
+//Build CA
+gulp.task('caholo', ['copyca'], function() {
+    //Build the CSS from the working directory
+    gulp.src( paths.ca.holo )
+    .pipe( hologram({logging:true}) );
+});
+
+//Copy all necessary CA files to the working directory
+gulp.task('copyca', function() {
+    return gulp.src( [paths.shared.src + "/**/*.scss", paths.ca.src + "/**/*.scss"] )
+        .pipe( gulp.dest( paths.ca.work ) )
+});
+
+//Build Keg
+gulp.task('kegholo', ['copykeg'], function() {
+    //Build the CSS from the working directory
+    gulp.src( paths.keg.holo )
+        .pipe( hologram({logging:true}) );
+});
+
+//Copy all necessary Keg files to the working directory
+gulp.task('copykeg', function() {
+    return gulp.src( [paths.shared.src + "/**/*.scss", paths.keg.src + "/**/*.scss"] )
+        .pipe( gulp.dest( paths.keg.work ) )
+});
+
+// Watch Files For Changes
+gulp.task('watch', function() {
+    gulp.watch( paths.shared.src, ['caholo','kegholo']);
+    gulp.watch( paths.ca.src, ['caholo']);
+    gulp.watch( paths.keg.src, ['kegholo']);
+});
+
+//default task that runs with command 'gulp' and calls other tasks specified
+gulp.task('default', function() {
+    gulp.start('caholo','kegholo');
+});
