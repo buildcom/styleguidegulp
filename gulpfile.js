88ea407606460534591dbe12f0c22b9ae8cb903d
Jean Page <jpage@JeanPageMac.local>
2016-08-21 13:38:26 -0500

    Adding Allergy and Air to Gulp build

--- gulpfile.js
+++ gulpfile.js
@@ -11,6 +11,11 @@ var paths = {
     shared: {
         src: './Source/Shared'
     },
+    aa: {
+        src: './Source/AAA',
+        work: './Working/AAA',
+        holo: './hologram_config_aa.yml'
+    },    
     ca: {
         src: './Source/CA',
         work: './Working/CA',
@@ -27,6 +32,20 @@ var onError = function (err) {
   gutil.beep();
   console.log(err);
 };
+//Build AAA
+gulp.task('aaholo', ['copyaa'], function() {
+    //Build the CSS from the working directory
+    gulp.src( paths.aa.holo )
+    .pipe( hologram({logging:true}) );
+});
+
+//Copy all necessary AAA files to the working directory
+gulp.task('copyaa', function() {
+    return gulp.src( [paths.shared.src + "/**/*.scss", paths.aa.src + "/**/*.scss"] )
+        .pipe( gulp.dest( paths.aa.work ) )
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./Destination/AAA'));
+});
 
 //Build CA
 gulp.task('caholo', ['copyca'], function() {
@@ -47,7 +66,7 @@ gulp.task('copyca', function() {
 gulp.task('kegholo', ['copykeg'], function() {
     //Build the CSS from the working directory  
     gulp.src( paths.keg.holo )
-        .pipe( hologram({logging:true}) );
+    .pipe( hologram({logging:true}) );
 });
 
 //Copy all necessary Keg files to the working directory
@@ -60,7 +79,8 @@ gulp.task('copykeg', function() {
 
 // Watch Files For Changes
 gulp.task('watch', function() {
-    gulp.watch( paths.shared.src, ['caholo','kegholo']);
+    gulp.watch( paths.shared.src, ['caholo','kegholo','aaholo']);
+    gulp.watch( paths.aa.src, ['aaholo']);
     gulp.watch( paths.ca.src, ['caholo']);
     gulp.watch( paths.keg.src, ['kegholo']);
     gulp.watch('Source/**/*.scss',['styles']);
@@ -68,5 +88,5 @@ gulp.task('watch', function() {
 
 //default task that runs with command 'gulp' and calls other tasks specified
 gulp.task('default', function() {
-    gulp.start('caholo','kegholo');
+    gulp.start('caholo','kegholo','aaholo');
 });
